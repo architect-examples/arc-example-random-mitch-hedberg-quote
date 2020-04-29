a0dd271c1425de8d427e82541232020a213c4563
Brian LeRoux <b@brian.io>
2020-04-29 10:13:39 -0700

    fix for response type

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -1,10 +1,10 @@
 const arc = require('@architect/functions')
 const quoth = require('random-mitch-hedberg-quote')
 
-function route(req, res) {
-  res({
+async function route(req) {
+  return {
     html: quoth()
-  })
+  }
 }
 
-exports.handler = arc.http(route)
+exports.handler = arc.http.async(route)
